558b7d37c68d065e46d8a6242e8a24faaa7ead3e
James Neale <42097394+james9226@users.noreply.github.com>
2023-05-15 00:25:15 +0100

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,61 @@
 const path = require('path');
 
-
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx,mdx}'),
-  path.join(__dirname, './components/**/*.{js,ts,jsx,tsx,mdx}'),
-  path.join(__dirname, './app/**/*.{js,ts,jsx,tsx,mdx}'),
-];
-export const theme = {
-  extend: {
-    fontFamily: {
-      satoshi: ['Satoshi', 'sans-serif'],
-      inter: ['Inter', 'sans-serif'],
+module.exports = {
+  purge: [
+    path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx,mdx}'),
+    path.join(__dirname, './components/**/*.{js,ts,jsx,tsx,mdx}'),
+    path.join(__dirname, './app/**/*.{js,ts,jsx,tsx,mdx}'),
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      },
     },
-    colors: {
-      'primary-orange': '#FF5722',
-    }
+  },
+  variants: {
+    extend: {},
   },
   daisyui: {
     themes: false,
   },
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/forms')
+  ],
 };
-export const plugins = [
-  require('@tailwindcss/forms'),
-  require("daisyui"),
-];
+
+// const path = require('path');
+
+
+// /** @type {import('tailwindcss').Config} */
+// export const content = [
+//   path.join(__dirname, './pages/**/*.{js,ts,jsx,tsx,mdx}'),
+//   path.join(__dirname, './components/**/*.{js,ts,jsx,tsx,mdx}'),
+//   path.join(__dirname, './app/**/*.{js,ts,jsx,tsx,mdx}'),
+// ];
+// export const theme = {
+//   extend: {
+//     fontFamily: {
+//       satoshi: ['Satoshi', 'sans-serif'],
+//       inter: ['Inter', 'sans-serif'],
+//     },
+//     colors: {
+//       'primary-orange': '#FF5722',
+//     }
+//   },
+//   daisyui: {
+//     themes: false,
+//   },
+//   plugins: [require("daisyui"), require('@tailwindcss/forms')],
+// };
+
+// export const plugins = [
+//   require('@tailwindcss/forms'),
+  // require("daisyui"),
+// ];
