ce38cdb931f217e9c9edfd9227c6704dc06a3ef2
James Neale <42097394+james9226@users.noreply.github.com>
2023-05-21 22:20:16 +0100

    Padding on privacy policy

--- app/(Navigation)/privacy-policy/page.tsx
+++ app/(Navigation)/privacy-policy/page.tsx
@@ -2,7 +2,7 @@ import { CheckCircleIcon } from '@heroicons/react/20/solid'
 
 export default function PrivacyPolicy() {
   return (
-    <div className="px-6 py-32 lg:px-8">
+    <div className="px-6 py-6 lg:px-8">
       <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <p className="text-base font-semibold leading-7 text-orange-600">Introducing</p>
         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Privacy Policy</h1>
